ee63b84b086905dbfe8012fdb13f159efcc8f7ee
Denis <carriere.denis@gmail.com>
2018-09-08 16:29:24 -0600

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,90 @@ import React from 'react';
 import './App.css';
 
 
-export default class App extends React.Component {
+export default class Container extends React.Component {
   render() {
     return (
-      <div className="email-receipt-email-receipt-1">
-          <div className="email-receipt-0" />
-          <div className="email-receipt-1">
-              <div className="email-receipt-1-0" />
-              <div className="email-receipt-path-1">
-                  <div className="email-receipt-1-1-0" />
-                  <div className="email-receipt-1-1-1">
-                      <div className="email-receipt-1-1-1-0" />
-                      <div className="email-receipt-body-8">
-                          <div className="email-receipt-1-1-1-1-0">
-                              <div className="email-receipt-1-1-1-1-0-0" />
-                              <div className="email-receipt-logo-6" />
-                              <div className="email-receipt-1-1-1-1-0-2" />
-                          </div>
-                          <div className="email-receipt-1-1-1-1-1">
-                              <div className="email-receipt-1-1-1-1-1-0" />
-                              <div className="email-receipt-logo-text-0">
-                                  eos receipt
-                              </div>
-                              <div className="email-receipt-1-1-1-1-1-2" />
-                          </div>
-                          <div className="email-receipt-1-1-1-1-2">
-                              <div className="email-receipt-1-1-1-1-2-0" />
-                              <div className="email-receipt-content-8">
-                                  Welcome to EOS Receipt where you can generate receipts for your EOS based crypto purchases. Search your transaction ID below.
-                              </div>
-                              <div className="email-receipt-1-1-1-1-2-2" />
-                          </div>
-                          <div className="email-receipt-1-1-1-1-3">
-                              <div className="email-receipt-1-1-1-1-3-0" />
-                              <div className="email-receipt-1-1-1-1-3-1">
-                                  <input type="text" placeholder="Search Tx ID..." className="email-receipt-search-field-5" />
-                              </div>
-                              <div className="email-receipt-1-1-1-1-3-2" />
-                          </div>
-                          <div className="email-receipt-1-1-1-1-4">
-                              <div className="email-receipt-1-1-1-1-4-0" />
-                              <div className="email-receipt-search-button-8">
-                                  <div className="email-receipt-1-1-1-1-4-1-0">
-                                      <div className="email-receipt-search-button-text-4">
-                                          SEARCH
-                                      </div>
-                                  </div>
-                              </div>
-                              <div className="email-receipt-1-1-1-1-4-2" />
-                          </div>
-                          <div className="email-receipt-1-1-1-1-5" />
-                      </div>
-                      <div className="email-receipt-1-1-1-2" />
+      <div className="container-container-1">
+          <div className="container-0" />
+          <div className="container-1">
+              <div className="container-1-0" />
+              <div className="container-body-1">
+                  <div className="container-1-1-0">
+                      <a href="https://eosreceipt.io" className="container-1-1-0-0">
+                          <div className="container-logo-7" />
+                      </a>
                   </div>
-                  <div className="email-receipt-1-1-2" />
+                  <div className="container-1-1-1">
+                      <div className="container-1-1-1-0" />
+                      <div className="container-logo-text-1">eos receipt</div>
+                      <div className="container-1-1-1-2" />
+                  </div>
+                  <div className="container-1-1-2">
+                      <div className="container-1-1-2-0" />
+                      <div className="container-content-8">
+                          Welcome to EOS Receipt where you can generate receipts for your EOS based crypto purchases. Search your transaction ID below.
+                      </div>
+                      <div className="container-1-1-2-2" />
+                  </div>
+                  <div className="container-1-1-3">
+                      <div className="container-1-1-3-0" />
+                      <div className="container-1-1-3-1">
+                          <input type="text" placeholder="Search Tx ID..." className="container-search-field-4" />
+                      </div>
+                      <div className="container-1-1-3-2" />
+                  </div>
+                  <div className="container-1-1-4">
+                      <div className="container-1-1-4-0" />
+                      <div className="container-search-button-1">
+                          <div className="container-1-1-4-1-0">
+                              <div className="container-search-button-text-8">
+                                  SEARCH
+                              </div>
+                          </div>
+                      </div>
+                      <div className="container-1-1-4-2" />
+                  </div>
+                  <div className="container-1-1-5" />
               </div>
-              <div className="email-receipt-1-2" />
+              <div className="container-1-2" />
           </div>
-          <div className="email-receipt-2" />
+          <div className="container-2" />
+          <div className="container-3">
+              <div className="container-3-0" />
+              <div className="container-sponsors-8">
+                  <div className="container-3-1-0">
+                      <div className="container-3-1-0-0" />
+                      <a href="http://www.uwyo.edu/uw/news/2018/06/blockchain-hackathon-event-poised-to-elevate-uws-technology-profile.html" target="_blank" className="container-3-1-0-1">
+                          <div className="container-uw-l-4" />
+                      </a>
+                      <div className="container-3-1-0-2" />
+                      <a href="https://www.eosnation.io/" target="_blank" className="container-3-1-0-3">
+                          <div className="container-eos-nation-logo-5" />
+                      </a>
+                      <div className="container-3-1-0-4" />
+                      <a href="https://eostribe.io" target="_blank" className="container-3-1-0-5">
+                          <div className="container-eos-tribe-logo-8" />
+                      </a>
+                      <div className="container-3-1-0-6" />
+                  </div>
+              </div>
+              <div className="container-3-2" />
+          </div>
+          <div className="container-4" />
+          <div className="container-5">
+              <div className="container-5-0" />
+              <div className="container-footer-2">
+                  <div className="container-5-1-0">
+                      <div className="container-5-1-0-0" />
+                      <div className="container-footer-text-3">
+                          A collaboration of EOS Tribe + EOS Nation at the 2018 Wyoming Hackathon
+                      </div>
+                      <div className="container-5-1-0-2" />
+                  </div>
+              </div>
+              <div className="container-5-2" />
+          </div>
+          <div className="container-6" />
       </div>
     );
   }
